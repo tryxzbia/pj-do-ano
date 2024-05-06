0090a1b12c75b8888b1a67f374ed8d59df89155f
Beatriz Correia De Araujo <108531051+tryxzbia@users.noreply.github.com>
2024-05-06 10:43:03 -0300

    s

--- main.js
+++ main.js
@@ -253,10 +253,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-05-30T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-15T00:00:00");
+const tempoObjetivo2 = new Date("2024-11-29T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-05T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-29T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
